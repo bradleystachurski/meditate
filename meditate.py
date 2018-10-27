f704a0ac57b1848205eb1b2c02117ae9c00198af
Bradley Stachurski <bradley.stachurski@gmail.com>
2018-10-27 06:34:00 -0600

    Working meditation timer

---
+++ meditate.py
@@ -0,0 +1,98 @@
+import argparse
+import os
+import subprocess
+import time
+from threading import Timer
+from typing import Callable
+
+from utils import StartableBackgroundThread
+
+parser = argparse.ArgumentParser(description="Minimal meditation CLI with a bell.")
+parser.add_argument("-m", "--minutes", default=10, type=int, help="Total minutes to meditate. To have an open ended meditation, set to 0. Defaults to 10.")
+parser.add_argument("-i", "--interval", default=2, type=int, help="Interval in minutes to ring meditation bell. Defaults to 2.")
+args = parser.parse_args()
+
+minutes = args.minutes * 60
+interval = args.interval * 60
+
+
+class MeditationTimer(StartableBackgroundThread):
+    """Meditation Timer"""
+
+    def __init__(self, total_time: int, interval_time: int):
+        super().__init__()
+        self._total_time = total_time
+        self._time_spent_meditating = 0
+        self._interval_time = interval_time
+        self._interval_timer = None
+
+    def _get_background_task_function(self) -> Callable:
+        return self._countdown_timer
+
+    def _countdown_timer(self):
+        # Untimed meditation => Infinite timer
+        if self._total_time == 0:
+            while True:
+                if self._interval_timer is None:
+                    self._interval_timer = IntervalTimer(self._interval_time)
+                    self._interval_timer.start()
+
+                mins, seconds = divmod(self._time_spent_meditating, 60)
+                timeformat = '{:02d}:{:02d}'.format(mins, seconds)
+                os.system('clear')
+                print(timeformat)
+                time.sleep(1)
+                self._time_spent_meditating += 1
+
+        # Timed meditation
+        while self._total_time:
+            if self._interval_timer is None:
+                self._interval_timer = IntervalTimer(self._interval_time)
+                self._interval_timer.start()
+
+            mins, seconds = divmod(self._total_time, 60)
+            timeformat = '{:02d}:{:02d}'.format(mins, seconds)
+            os.system('clear')
+            print(timeformat)
+            time.sleep(1)
+            self._total_time -= 1
+
+
+class IntervalTimer(StartableBackgroundThread):
+    """Interval Timer. Rings a bell after a given interval."""
+
+    def __init__(self, interval_time: int):
+        super().__init__()
+        self._original_interval = interval_time
+        self._remaining_interval_time = interval_time
+
+    def _get_background_task_function(self) -> Callable:
+        return self._interval_timer
+
+    def _interval_timer(self):
+        while self._remaining_interval_time:
+            if self._remaining_interval_time == 1:
+                time.sleep(1)
+                bell_sound = PlayBellSound()
+                bell_sound.start()
+                self._remaining_interval_time = self._original_interval
+            time.sleep(1)
+            self._remaining_interval_time -= 1
+
+
+class PlayBellSound(StartableBackgroundThread):
+    """Plays the bell sound in a background process."""
+
+    def __init__(self):
+        super().__init__()
+        self._audio_path = os.path.abspath("assets/bowl.mp3")
+
+    def _get_background_task_function(self) -> Callable:
+        return self._play_audio
+
+    def _play_audio(self):
+        subprocess.call(["afplay", self._audio_path])
+
+
+meditation_timer = MeditationTimer(minutes, interval)
+meditation_timer.start()
---
+++ utils.py
@@ -0,0 +1,54 @@
+from abc import ABC, abstractmethod
+from threading import Thread
+from typing import Callable
+
+
+class Startable:
+    def __init__(self):
+        self._started = False
+
+    def is_started(self) -> bool:
+        return self._started
+
+    def start(self) -> bool:
+        if self._started:
+            return False
+        self._started = True
+        return True
+
+    def stop(self) -> bool:
+        if not self._started:
+            return False
+        self._started = False
+        return True
+
+
+class StartableBackgroundThread(ABC, Startable):
+    """Base class that creates a Thread on start() that runs the background task returned
+    by _get_background_task_function(). This function will only be called once.
+
+    Attributes:
+        _thread: The handle for the Thread used for the background task
+    """
+    def __init__(self):
+        super().__init__()
+        self._thread = None
+
+    def start(self) -> bool:
+        if super().start():
+            self._thread = Thread(target=self._get_background_task_function())
+            self._thread.start()
+            return True
+        return False
+
+    def stop(self) -> bool:
+        if super().stop():
+            self._thread.join(.05)
+            return True
+        return False
+
+    @abstractmethod
+    def _get_background_task_function(self) -> Callable:
+        """The abstract method that is called to get the function the Thread should run"""
+        pass
+
